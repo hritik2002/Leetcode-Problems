440af5c05fe1fc4d33d4f33e835cbf19d644918e
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-04-16 09:46:08 +0530

    Time: 35 ms (91.20%), Space: 33.5 MB (19.45%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -10,25 +10,18 @@
  * };
  */
 class Solution {
+    int cur_sum = 0 ;
 public:
-    void dfs(TreeNode* root , map<int , int> &storeSum,int& sum){
-        if(root==NULL)return;
-        dfs(root->right,storeSum,sum);
-        sum += root->val ;
-        storeSum[root->val] = sum ;
-        dfs(root->left,storeSum,sum);
-    }
-    void dfs2(TreeNode* root , map<int,int>&storeSum){
-        if(root==NULL)return ;
-        root->val = storeSum[root->val];
-        dfs2(root->left,storeSum);
-        dfs2(root->right,storeSum);
+    void dfs(TreeNode* root ){
+        if (!root) return;
+        if (root->right) dfs(root->right);
+        
+        root->val = (cur_sum += root->val);
+        if (root->left) dfs(root->left);
     }
     TreeNode* convertBST(TreeNode* root) {
-        map<int , int > storeSum ;
-        int sum = 0 ;
-        dfs(root,storeSum,sum);
-        dfs2(root,storeSum);
+        
+        dfs(root);
         return root;
     }
 };
