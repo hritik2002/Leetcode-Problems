4b71657a6c0528296a5c599bd8e32c71a3b0f011
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-05-03 12:35:05 +0530

    Time: 16 ms (12.16%), Space: 12.5 MB (67.38%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
@@ -3,9 +3,10 @@ public:
     int findGCD(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int first = nums[0] , second = nums[nums.size()-1];
-        while(first!=second){
-            if(first>second)first-=second;
-            else second -= first ;
+        while(second!=0){
+            int temp = first ;
+            first = second ;
+            second = temp%second;
         }
         return first;
     }
