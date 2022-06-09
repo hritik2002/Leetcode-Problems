1b45c4961c3b7c94bdde6ef037d643aa66a4d49f
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-06-09 10:15:12 +0530

    Time: 38 ms (5.30%), Space: 15.9 MB (52.41%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int>ans;
-        map<int,int>store;
+        int val1 = 0 , val2 = 0 , count1 = 0 , count2 = 0 ;
         int n = nums.size();
-        for(int i=0 ; i<n ; i++){
-            if(store[nums[i]]){
-                store[nums[i]]++;
-            }else{
-                store[nums[i]] = 1 ;
-            }
-            if(store[nums[i]]>n/3){
-                ans.push_back(nums[i]);
-                store[nums[i]] = INT_MIN ;
-            }
+        for(auto x:nums){
+            if(x==val1)count1++,val1=x;
+            else if(x==val2)count2++,val2=x;
+            else if(count2==0)count2=1,val2=x;
+            else if(count1==0)count1=1,val1=x;
+            else count1--,count2--;
         }
+        count1=0,count2=0;
+        for(auto x:nums){
+            if(x==val1)count1++;
+            else if(x==val2)count2++;
+        }
+        vector<int>ans;
+        if(count1>n/3)ans.push_back(val1);
+        if(count2>n/3)ans.push_back(val2);
         return ans;
     }
 };
