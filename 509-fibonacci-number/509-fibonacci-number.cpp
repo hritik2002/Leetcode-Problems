de52ba90a640d0f620b831cd10a1f0257903165b
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-06-11 12:47:40 +0530

    Time: 0 ms (100.00%), Space: 6 MB (41.54%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -3,6 +3,12 @@ public:
     int fib(int n) {
         if(n==0)return 0;
         if(n<=2)return 1;
-        return fib(n-1)+fib(n-2);
+        int a = 1,b = 1, c = 1;
+        for(int i=2;i<n;i++){
+            c = a + b;
+            a = b ;
+            b = c ;
+        }
+        return c;
     }
 };
