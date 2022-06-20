bed7bcf0386f218760a33831c6e43f8724ecc009
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-06-20 10:27:50 +0530

    Time: 998 ms (5.03%), Space: 83 MB (40.73%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int _min = INT_MAX;
-        set<int>st;
-        int n  = nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]>n)continue;
-            if(nums[i]>0){
-                st.insert(nums[i]);
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(nums[i]>0){
-                if((!st.count(nums[i]-1) && nums[i]>1)){
-                    _min = min(_min,nums[i]-1);
-                }else if(!st.count(nums[i]+1)){
-                    _min = min(_min,nums[i]+1);
+        int n = nums.size();
+        int _max = 1;
+        for(int i=0;i<n;){
+            if(nums[i]>0 && nums[i]<=n){
+                _max = max(_max,nums[i]);
+                while(nums[i]>0 && nums[i]<=n && nums[nums[i]-1]!=nums[i]){
+                    int temp = nums[nums[i]-1];
+                    nums[nums[i]-1] = nums[i];
+                    nums[i] = temp ;
                 }
-            }
+            } 
+            i++;
         }
-        if(!st.count(1))return 1;
-        return _min;
+        for(int i=0;i<n;i++)cout<<nums[i]<<" ";
+        for(int i=0;i<n;i++){
+            if( i+1!=nums[i])return i+1;
+        }
+        return _max+1;
     }
 };
