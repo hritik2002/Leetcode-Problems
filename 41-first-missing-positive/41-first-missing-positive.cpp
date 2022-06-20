2910aae3a47f940db90d5d86ab75f2807013e594
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-06-20 10:01:02 +0530

    Time: 961 ms (5.03%), Space: 139 MB (5.89%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n = nums.size()-1;
         int _min = INT_MAX;
-        bool flag = true;
-        for(int i=0; i<nums.size();i++){
+        set<int>st;
+        int n  = nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]>n)continue;
             if(nums[i]>0){
-                if(i+1<=n &&nums[i]!=nums[i+1] && nums[i]!=nums[i+1]-1){
+                st.insert(nums[i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                if((!st.count(nums[i]-1) && nums[i]>1)){
+                    _min = min(_min,nums[i]-1);
+                }else if(!st.count(nums[i]+1)){
                     _min = min(_min,nums[i]+1);
                 }
             }
-            if(nums[i]==1)flag=false;
         }
-        if(flag)return 1;
-        if(_min<0)return 1;
-        if(nums[0]>1)_min = min(_min,nums[0]-1);
-        if(nums[n] <INT_MAX-1)
-            _min = min(_min,nums[n]+1);
+        if(!st.count(1))return 1;
         return _min;
     }
 };
