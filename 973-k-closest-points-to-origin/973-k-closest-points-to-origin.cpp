2f03a122550b0fa83d8e661cdb82b8ef28a9357c
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-06-23 21:41:14 +0530

    Time: 382 ms (46.42%), Space: 63 MB (69.29%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -2,17 +2,12 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         int n = points.size();
-        vector<double>distance(n,0);
-        for(int i=0;i<n;i++){
-            double square_sum = (points[i][0]*points[i][0])+(points[i][1]*points[i][1]);
-            double dist =  sqrt(square_sum);
-            distance[i] = dist ;
-        }
         vector<vector<int>>ans;
         vector<pair<double,int>>dist(n);
         for(int i=0;i<n;i++){
             pair<double,int>temp ;
-            temp.first = distance[i];
+             double square_sum = (points[i][0]*points[i][0])+(points[i][1]*points[i][1]);
+            temp.first = sqrt(square_sum);
             temp.second = i ;
             dist[i] = temp ;
         }
