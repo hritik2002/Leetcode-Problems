18104848fd3f4b43529383199a6978b4e8854adf
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-06-25 12:47:58 +0530

    Time: 62 ms (9.80%), Space: 27.1 MB (53.59%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size(),count = 0;
         for(int i=1;i<n;i++){
             if(nums[i-1]>nums[i]){
-                if(i==1 || (i-1>0 && nums[i-2]<=nums[i])){
+                if(i==1 || nums[i-2]<=nums[i]){
                     nums[i-1] = nums[i];
                 }else {
                     nums[i] = nums[i-1];
