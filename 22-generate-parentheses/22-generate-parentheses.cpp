51f8fcc5bb3c97a477f4a933cb1e2b0385441964
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-06-27 19:19:58 +0530

    Time: 11 ms (26.91%), Space: 16 MB (12.31%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
     vector<string>ans;
-    bool isValid(string s,int n){
-        // return true;
-        if(n>0 && s[0]==')' || s[n-1]=='(')return false;
-        stack<char>stck;
-        stck.push(s[0]);
-        for(int i=1;i<n;i++){
-            if(s[i]==')'){
-                if(stck.top()=='('){
-                    stck.pop();
-                }else{
-                    stck.push(s[i]);
-                }
-            }else{
-                stck.push(s[i]);
-            }
-        }
-        if(!stck.empty())return false;
-        return true;
-    }
     void generateParenthesis(int n,string s,int open,int close){
         if(open==0 && close ==0){
             ans.push_back(s);
