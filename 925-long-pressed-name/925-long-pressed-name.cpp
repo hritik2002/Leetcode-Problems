f72f78185d406de1c8dfe022a834c670d26b88d1
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-07-13 12:12:48 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (13.21%) - LeetHub

--- 925-long-pressed-name/925-long-pressed-name.cpp
+++ 925-long-pressed-name/925-long-pressed-name.cpp
@@ -3,12 +3,12 @@ public:
     bool isLongPressedName(string name, string typed) {
         int i = 0, j = 0 ;
         int name_len = name.length(), typed_len = typed.length();
-        while(i<name_len && j<typed_len){
-            if(name[i]==typed[j]){
+        while(j<typed_len){
+            if(i<name_len && name[i]==typed[j]){
                 i++;
                 j++;
             }else{
-                if(i>0 && name[i-1] == typed[j]);
+                if(i<=name_len && i>0 && name[i-1] == typed[j]);
                 else{
                     return false;
                 }
@@ -16,10 +16,6 @@ public:
             }
         }
         if(i != name_len)return false;
-        while(j<typed_len){
-            if(name[i-1] != typed[j])return false;
-            j++;
-        }
         return true;
         
     }
