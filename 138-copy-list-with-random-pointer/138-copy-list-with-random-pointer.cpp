2b178408d27a335d664f3156b4faea9da2dfafd6
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-07-30 10:14:42 +0530

    Time: 20 ms (20.07%), Space: 11.5 MB (16.62%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -19,14 +19,10 @@ public:
     Node* copyRandomList(Node* head) {
         if(head == NULL)return head;
         
-        int index = 0;
         Node* init_head = head;
-        map<int, Node*> int_to_node;
         map<Node*, Node*> node_to_node;
         
         Node* newHead  = new Node(head->val);
-        // int_to_node[index] = head->random;
-        // index++;
         node_to_node[head] = newHead;
         node_to_node[NULL] = NULL;
         
@@ -35,23 +31,18 @@ public:
         
         while(head){
             tail->next = new Node(head->val);
-            // int_to_node[index] = head->random;
             node_to_node[head] = tail->next;
-            // index++;
-            
             head = head->next;
             tail = tail->next;
         }
         
         tail = newHead;
         head = init_head;
-        index = 0;
         
         while(tail){
             tail->random = node_to_node[head->random];
             tail = tail->next;
             head = head->next;
-            // index++;
         }
         
         return newHead;
