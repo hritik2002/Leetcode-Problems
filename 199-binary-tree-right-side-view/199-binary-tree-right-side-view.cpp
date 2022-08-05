223f4e9d535b752cb05e511db06b90b0434f3027
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-08-06 03:49:19 +0530

    Time: 3 ms (85.34%), Space: 11.9 MB (66.60%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -14,7 +14,9 @@ public:
     void recursion(TreeNode *root, int level, vector<int> &res)
     {
         if(root==NULL) return ;
+        
         if(res.size()<level) res.push_back(root->val);
+        
         recursion(root->right, level+1, res);
         recursion(root->left, level+1, res);
     }
