6d8a1be8a3a3468a5ed2b7b2c85e93a808465332
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-08-07 13:47:39 +0530

    Time: 8 ms (57.62%), Space: 11.8 MB (97.06%) - LeetHub

---
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int, vector<int>> verticalLevelNodes;
+        
+        queue<pair<TreeNode*, int>> queueNode;
+        queueNode.push({root, 0});
+        
+        while(queueNode.size() > 0) {
+            int size = queueNode.size();
+            map<int, vector<int>> rowNodes;
+            
+            for(int i = 0; i < size; i++) {
+                pair<TreeNode*, int> front = queueNode.front();
+                queueNode.pop();
+                rowNodes[front.second].push_back(front.first->val);
+                int level = front.second;
+                
+                if(rowNodes[front.second].size() > 1) {
+                    sort(rowNodes[front.second].begin(), rowNodes[front.second].end());
+                }
+                
+                if(front.first->left) {
+                    queueNode.push({front.first->left, level - 1});
+                }
+                if(front.first->right) {
+                    queueNode.push({front.first->right, level + 1});
+                }
+            }
+            
+            for(auto row : rowNodes) {
+                if(row.second.size() > 1) {
+                    for(auto val : row.second) {
+                        verticalLevelNodes[row.first].push_back(val);
+                    }
+                }else {
+                    verticalLevelNodes[row.first].push_back(row.second[0]);
+                }
+            }
+            
+        }
+        
+        vector<vector<int>> res;
+        
+        for(auto verticalLevel : verticalLevelNodes) {
+            res.push_back(verticalLevel.second);
+        }
+        
+        return res;
+    }
+};
