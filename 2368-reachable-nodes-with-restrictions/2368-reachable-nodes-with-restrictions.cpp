f8aafc08ee459b99cf1a6b944cf58be18c052f8d
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-08-11 17:03:39 +0530

    Time: 754 ms (22.22%), Space: 166.9 MB (11.11%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
-    void dfs(int u, vector<int> adj[], int &cnt, vector<bool> &vis)
-    {
-        vis[u] = true;
-        // if we reach a new node, while performing dfs, we increase our count by 1
-        cnt++; 
-        for (auto v : adj[u])
-        {
-            if (vis[v] == false)
-            {
-                dfs(v, adj, cnt, vis);
+    
+    int count = 0;
+    
+    void dfs(vector<bool> &visited, vector<int> adj[], int curr) {
+        
+        count++;
+        visited[curr] = true;
+        
+        for(auto node : adj[curr]) {
+            if(visited[node]) {
+                continue;
             }
+            dfs(visited, adj, node);
         }
+        
+        return;
     }
-    int reachableNodes(int n, vector<vector<int>> &edges, vector<int> &restricted)
-    {
+    int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
+        vector<int> adj[n];
         vector<bool> visited(n, false);
-        vector<int> adj[n]; // adjency list
-        for (int i = 0; i < edges.size(); i++)
-        {
+        
+        for(auto num : restricted) {
+            visited[num] = true;
+        }
+        
+        for(int i = 0; i < n - 1; i++) {
             adj[edges[i][0]].push_back(edges[i][1]);
             adj[edges[i][1]].push_back(edges[i][0]);
         }
-        for (auto a : restricted)
-        {
-            visited[a] = true; // marking the restricted nodes  as visited.
-        }
-        int count = 0;
-        dfs(0, adj, count, visited);
+        
+        dfs(visited, adj, 0);
+        
         return count;
     }
 };
