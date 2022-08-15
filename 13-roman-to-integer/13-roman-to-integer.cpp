8bcd381203bb3da0f6fd03d9c9cdf89ee67b01d3
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-08-15 13:58:26 +0530

    Time: 15 ms (62.96%), Space: 8.2 MB (30.55%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -2,14 +2,7 @@ class Solution {
 public:
     int romanToInt(string s) {
         map<char, int> mp;
-        map<char, int> rank;
-        rank['I'] = 1;
-        rank['V'] = 2;
-        rank['X'] = 10;
-        rank['L'] = 50;
-        rank['C'] = 100;
-        rank['D'] = 500;
-        rank['M'] = 1000;
+
         
         mp['I'] = 1;
         mp['V'] = 5;
@@ -24,7 +17,7 @@ public:
         long long int ans = 0;
         
         for(int i = n - 1; i >= 0; i--) {
-            if(i >= 1 && (rank[s[i]] > rank[s[i - 1]] ))  {
+            if(i >= 1 && (mp[s[i]] > mp[s[i - 1]] ))  {
                 ans += mp[s[i]] - mp[s[i - 1]];
                 i--;
             }else {
