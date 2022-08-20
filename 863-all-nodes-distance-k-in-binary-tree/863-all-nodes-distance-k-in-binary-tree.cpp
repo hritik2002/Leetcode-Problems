bab93ecf4a30f25a295bbe1fd6d105495df600ab
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-08-20 19:41:41 +0530

    Time: 3 ms (95.80%), Space: 18.7 MB (5.07%) - LeetHub

---
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,73 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> res;
+    set<TreeNode*> st;
+    // 1) Find the target node
+    // 2) Search for nodes at distance k
+    
+    void findTarget(TreeNode* root, TreeNode* target, int k, vector<TreeNode*> parentNodes) {
+        if(root == NULL) {
+            return;
+        }
+        if(root == target) {
+            if(k == 0) {
+                res.push_back(root->val);
+                
+                return;
+            }
+            st.insert(root);
+            
+            nodesAtKDistance(root->left, k - 1);
+            nodesAtKDistance(root->right, k - 1);
+            
+            int n = parentNodes.size();
+            
+            for(int i = n - 1; i >= 0; i--) {
+                nodesAtKDistance(parentNodes[i], k - n + i);
+                st.insert(parentNodes[i]);
+            }
+            
+            return;
+        }
+        
+        parentNodes.push_back(root);
+        
+        findTarget(root->left, target, k, parentNodes);
+        findTarget(root->right, target, k, parentNodes);
+        
+        return;
+    }
+    
+    void nodesAtKDistance(TreeNode* root, int k) {
+        if(root == NULL || k < 0 || st.count(root)) {
+            return;
+        }
+        if(k == 0) {
+            st.insert(root);
+            res.push_back(root->val);
+            
+            return;
+        }
+        
+        nodesAtKDistance(root->left, k - 1);
+        nodesAtKDistance(root->right, k - 1);
+        
+        return;
+    }
+    
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        findTarget(root, target, k, {});
+        
+        return res;
+    }
+};
