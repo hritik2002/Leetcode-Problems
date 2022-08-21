41df646df3425a9ec4d7b9bcb3e362f3ae263384
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-08-21 14:56:50 +0530

    Time: 4 ms (85.21%), Space: 16.3 MB (93.71%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -11,67 +11,33 @@
  */
 class Solution {
 public:
-    bool isSymmetric(TreeNode* root) {
-        return root == NULL || getIsSymmetric(root->left, root->right);
-    }
-    
-    bool getIsSymmetric(TreeNode* left, TreeNode* right) {
-        if(left == NULL && right == NULL)return true;
-        if(left == NULL || right == NULL)return false;
-        
-        if(left->val != right->val)
+    bool getSymmetric(TreeNode* left, TreeNode* right) {
+        if(left == NULL && right == NULL) {
+            return true;
+        }
+        if(left == NULL || right == NULL) {
             return false;
+        }
+        if(left->val != right->val) {
+            return false;
+        }
         
-        return getIsSymmetric(left->left, right->right) && getIsSymmetric(left->right, right->left);
+        bool leftChild =  getSymmetric(left->left, right->right);
+        
+        if(leftChild == false) {
+            return false;
+        }
+        
+        bool rightChild = getSymmetric(left->right, right->left);
+        
+        return leftChild && rightChild;
     }
-    
-    
-//     bool isSymmetric(TreeNode* root) {
-//         if(root == NULL)return true;
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL) {
+            return true;
+        }
         
-        
-//         queue<TreeNode*> nodeQueue;
-//         nodeQueue.push(root);
-//         nodeQueue.push(NULL);
-//         vector<int> rowNodes;
-        
-//         while(nodeQueue.size()){
-//             TreeNode* front = nodeQueue.front();
-//             nodeQueue.pop();
-            
-//             if(front == NULL){
-//                 if(nodeQueue.size() > 0){
-//                     nodeQueue.push(NULL);
-//                 }
-                
-//                 int n = rowNodes.size();
-//                 int i = 0, j = n - 1;
-                
-//                 while(i <= j){
-//                     if(rowNodes[i] != rowNodes[j])
-//                         return false;
-//                     i++;
-//                     j--;
-//                 }
-                
-//                 vector<int> temp;
-//                 rowNodes = temp;
-//             }else{
-//                 if(front->left){
-//                     nodeQueue.push(front->left);
-//                     rowNodes.push_back(front->left->val);
-//                 }else{
-//                     rowNodes.push_back(INT_MIN);
-//                 }
-//                 if(front->right){
-//                     nodeQueue.push(front->right);
-//                     rowNodes.push_back(front->right->val);
-//                 }else{
-//                     rowNodes.push_back(INT_MIN);
-//                 }
-//             }
-//         }
-        
-//         return true;
-//     }
+        return getSymmetric(root->left, root->right);
+    }
+   
 };
