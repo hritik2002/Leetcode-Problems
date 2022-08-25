b9e494248c3deac277cbe1ac0f27c52b8887aa15
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-08-25 09:04:17 +0530

    Time: 2217 ms (32.73%), Space: 623 MB (16.73%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -40,20 +40,22 @@ public:
     }
     
     bool search(const char* word, TrieNode* node) {
-        for (int i = 0; word[i] && node; i++) {
-            if (word[i] != '.') {
+        for(int i = 0; word[i] && node; i++) {
+            if(word[i] != '.') {
                 node = node -> children[word[i] - 'a'];
-            } else {
-                TrieNode* tmp = node;
-                
-                for (int j = 0; j < 26; j++) {
-                    node = tmp -> children[j];
-                    if (search(word + i + 1, node)) {
+            }else {
+                TrieNode* temp = node;
+            
+                for(int j = 0; j < 26; j++) {
+                    node = temp -> children[j];
+                    
+                    if(search(word + i + 1, node)) {
                         return true;
                     }
                 }
             }
         }
+        
         return node && node -> is_word;
     }
 
