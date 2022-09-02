3095c940bad990f127b65e3fc3aa18b87da63285
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-09-02 14:29:13 +0530

    Time: 388 ms (55.43%), Space: 78.4 MB (26.87%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,22 +1,26 @@
+#define pi pair<double, vector<int>>
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        int n = points.size();
-        vector<vector<int>>ans;
-        vector<pair<double,int>>dist(n);
-        for(int i=0;i<n;i++){
-            pair<double,int>temp ;
-             double square_sum = (points[i][0]*points[i][0])+(points[i][1]*points[i][1]);
-            temp.first = sqrt(square_sum);
-            temp.second = i ;
-            dist[i] = temp ;
+        priority_queue<pi> pq;
+        vector<vector<int>> ans;
+        
+        for(auto arr: points) {
+            double dist = sqrt(arr[0]*arr[0] + arr[1]*arr[1]);
+            
+            if(pq.size() < k) {
+                pq.push({dist, arr});
+            }else if(dist < pq.top().first) {
+                pq.pop();
+                pq.push({dist, arr});
+            }
         }
-        sort(dist.begin(),dist.end());
-        int i = 0 ;
-        while(k--){
-            ans.push_back(points[dist[i].second]);
-            i++;
+        
+        while(pq.size()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
+        
         return ans;
     }
 };
