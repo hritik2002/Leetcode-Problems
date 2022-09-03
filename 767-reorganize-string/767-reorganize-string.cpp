034366e865a3a0ef28231c89f8157f6b55c3f92b
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-09-03 13:56:50 +0530

    Time: 5 ms (37.92%), Space: 6.2 MB (67.85%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        priority_queue<pair<int, char>> maxHeap;
+        map<char, int> mp;
+        int len = s.length();
+        
+        for(int i = 0; i < len; i++) {
+            mp[s[i]]++;
+        }
+        
+        for(auto x : mp) {
+            maxHeap.push({x.second, x.first});
+        }
+        
+        string ans = "";
+        
+        while(maxHeap.size() > 1) {
+            auto top = maxHeap.top();
+            maxHeap.pop();
+            auto top2 = maxHeap.top();
+            maxHeap.pop();
+            
+            if(top.first > 1) {
+                maxHeap.push({top.first - 1, top.second});
+            }
+            if(top2.first > 1) {
+                maxHeap.push({top2.first - 1, top2.second});
+            }
+            
+            ans += top.second;
+            ans += top2.second;
+            
+        }
+        
+        if(maxHeap.size()) {
+            if(maxHeap.top().first > 1)
+                return "";
+            else ans += maxHeap.top().second;
+            
+        }
+        
+        // cout<<ans<<endl;
+        
+        return ans;
+    }
+};
