b2f9b8d598c971da6ef2f9518b6e4ee0178f519e
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-09-14 17:23:39 +0530

    Time: 238 ms (14.87%), Space: 54.1 MB (70.98%) - LeetHub

--- 1936-add-minimum-number-of-rungs/1936-add-minimum-number-of-rungs.cpp
+++ 1936-add-minimum-number-of-rungs/1936-add-minimum-number-of-rungs.cpp
@@ -4,10 +4,7 @@ public:
         int step = 0, n = rungs.size(), stepsAdded = 0;
         
         for(int i = 0; i < n; i++) {
-            if(abs(rungs[i] - step) > dist) {
-                stepsAdded += (rungs[i] - step - 1) / dist;
-                
-            }
+            stepsAdded += (rungs[i] - step - 1) / dist;
             
             step = rungs[i];
         }
