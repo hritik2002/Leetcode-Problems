4fb194e14bb5da9d2dd24b7e3d115a52317ce9f7
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-09-16 22:01:13 +0530

    Time: 193 ms (39.86%), Space: 46.7 MB (85.65%) - LeetHub

--- 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.js
+++ 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.js
@@ -3,40 +3,17 @@
  * @param {number} k
  * @return {number[]}
  */
-var addToArrayForm = function(num, k) {
-    const num_to_add = k.toString().split("");
-    
-    let i = num.length - 1, j = num_to_add.length - 1;
-    let carry = 0;
-    
-    while(i >= 0 && j >= 0) {
-        const sum = (num[i] + Number(num_to_add[j]) + carry);
-        carry = Math.floor((sum) / 10);
-        num[i] = sum % 10;
-        
-        i--;
-        j--;
+var addToArrayForm = function(num, K) {
+     for (let i = num.length - 1; i >= 0 && K > 0; --i) {
+            num[i] += K;
+            K = Math.floor(num[i] / 10);
+            num[i] %= 10;
     }
     
-    while(i >= 0 && carry) {
-        const sum = (num[i] + carry);
-        carry = Math.floor((sum) / 10);
-        num[i] = sum % 10;
-        
-        i--;
+    while (K > 0) {
+        num = [K % 10, ...num];
+        K = Math.floor(K / 10);
     }
-    
-    while(j >= 0) {
-        const sum = (Number(num_to_add[j]) + carry);
-        carry = Math.floor((sum) / 10);
-        num = [sum % 10, ...num];
-        
-        j--;
-    }
-    
-    if(carry) {
-        num = [carry, ...num];
-    }
-    
+
     return num;
 };
