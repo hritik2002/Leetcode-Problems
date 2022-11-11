1b9016a11e0c52e003593ada44688fdd00200aff
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-11-11 12:19:02 +0530

    Time: 1169 ms (5.00%), Space: 126.9 MB (5.08%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
+    int dp[32][32];
     bool solve(string s, string p, int i, int j, int n, int m) {
         if(j == m) {
             return i == n;
         }
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
         
         if(p[j + 1] == '*') {
             if(solve(s, p, i, j + 2, n, m)) {       // assuming * -> null
-                return 1;
+                return dp[i][j] = 1;
             }
             while(i < n && (s[i] == p[j] || p[j] == '.')) {
-                if(solve(s, p, ++i, j + 2, n, m)) return 1;
+                if(solve(s, p, ++i, j + 2, n, m)) return dp[i][j] = 1;
             }
         }else if(i < n && (s[i] == p[j] || p[j] == '.') && solve(s, p, i + 1, j + 1, n, m)) {
-            return 1;
+            return dp[i][j] = 1;
         }
         
-        return 0;
+        return dp[i][j] = 0;
     }
     bool isMatch(string s, string p) {
+        memset(dp, -1, sizeof(dp));
         int n = s.length(), m = p.length();
         return solve(s, p, 0, 0, n, m);
     }
