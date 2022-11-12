a6a291b4f7127cc58a77bae0c5a98dc493da4927
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2022-11-12 10:07:09 +0530

    Time: 191 ms (18.06%), Space: 11.3 MB (64.00%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -27,25 +27,4 @@ public:
         
         return  dp[0][0];
     }  
-    bool helper(string s, string p,int i,int j,vector<vector<int>> &v)
-    {
-        if(j==p.length())
-            return (i==s.length());
-        if(v[i][j]<0){
-            if(i==s.length())
-                v[i][j] = (p[j]=='*' && helper(s,p,i,j+1,v));
-            else if(i<s.length() &&  (p[j]==s[i] || p[j]=='?'))
-            {
-               v[i][j]=  helper(s,p,i+1,j+1,v);
-            }
-            else if(p[j]=='*')
-            {
-                v[i][j]= (helper(s,p,i,j+1,v) || helper(s,p,i+1,j,v));
-            }
-            else
-                v[i][j]= false;
-        }
-        
-        return v[i][j];
-    }
 };
