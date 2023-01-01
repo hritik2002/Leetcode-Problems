5a6ef0a59081e0a7adbc4ad7da81677299995437
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2023-01-01 21:29:34 +0530

    Time: 64 ms (81.99%), Space: 41.5 MB (95.31%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+    let obj = {};
+    let word_to_letter = {}
+    s = s.split(" ");
+    if(s.length != pattern.length) {
+        return false;
+    }
+    
+    for(let i = 0; i < pattern.length; i++) {
+        if(!obj[pattern[i]]) {
+            if(word_to_letter[s[i]]) {
+                return false;
+            }
+            
+            word_to_letter[s[i]] = pattern[i];
+            obj[pattern[i]] = s[i];
+        }else {
+            if(word_to_letter[s[i]] != pattern[i]) {
+                return false;
+            }
+            if(obj[pattern[i]] != s[i]) {
+                return false;
+            }
+        }
+    }
+    
+    return true;
+};
