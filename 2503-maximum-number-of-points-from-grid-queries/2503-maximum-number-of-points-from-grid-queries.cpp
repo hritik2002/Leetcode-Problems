0352d1d77aee2e72d28170920958d7326f64c1b3
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2023-01-04 14:19:31 +0530

    Time: 488 ms (62.99%), Space: 35.4 MB (75.27%) - LeetHub

--- 2503-maximum-number-of-points-from-grid-queries/2503-maximum-number-of-points-from-grid-queries.cpp
+++ 2503-maximum-number-of-points-from-grid-queries/2503-maximum-number-of-points-from-grid-queries.cpp
@@ -13,6 +13,7 @@ public:
         sort(q.begin(), q.end());
         priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
         pq.push({grid[0][0], {0, 0}});
+        visited[0][0] = true;
         
         for(auto query : q) {
             
@@ -22,26 +23,25 @@ public:
                 int x = top.second.first, y = top.second.second;
                 int value = top.first;
                 
-                if(visited[x][y]) {
-                    continue;
-                }
-                visited[x][y] = true;
-                
                 // down
                 if(x + 1 < n && !visited[x + 1][y]) {
                     pq.push({grid[x + 1][y], {x + 1, y}});
+                    visited[x + 1][y] = true;
                 }
                 // up
                 if(x - 1 >= 0 && !visited[x - 1][y]) {
                     pq.push({grid[x - 1][y], {x - 1, y}});
+                    visited[x - 1][y] = true;
                 }
                 // left
                 if(y - 1 >= 0 && !visited[x][y - 1]) {
                     pq.push({grid[x][y - 1], {x, y - 1}});
+                    visited[x][y - 1] = true;
                 }
                 // right
                 if(y + 1 < m && !visited[x][y + 1]) {
                     pq.push({grid[x][y + 1], {x, y + 1}});
+                    visited[x][y + 1] = true;
                 }
                 
                 cnt++;
