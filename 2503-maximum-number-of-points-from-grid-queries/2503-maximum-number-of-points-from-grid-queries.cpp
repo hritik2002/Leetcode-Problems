1bff137f174e9c356dafbf374ecc3d1be65d59c3
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2023-01-04 13:44:04 +0530

    Time: 519 ms (59.79%), Space: 40.3 MB (61.70%) - LeetHub

---
+++ 2503-maximum-number-of-points-from-grid-queries/2503-maximum-number-of-points-from-grid-queries.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
+        vector<int> ans(queries.size());
+        int cnt = 0;
+        int n = grid.size(), m = grid[0].size();
+        vector<pair<int, int>> q;
+        vector<vector<bool>> visited(n, vector<bool> (m, false));
+        
+        for(int i = 0; i < queries.size(); i++) {
+            q.push_back({queries[i], i});
+        }
+        sort(q.begin(), q.end());
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        pq.push({grid[0][0], {0, 0}});
+        
+        for(auto query : q) {
+            
+            while(!pq.empty() && pq.top().first < query.first) {
+                auto top = pq.top();
+                pq.pop();
+                int x = top.second.first, y = top.second.second;
+                int value = top.first;
+                
+                if(visited[x][y]) {
+                    continue;
+                }
+                visited[x][y] = true;
+                
+                // down
+                if(x + 1 < n && !visited[x + 1][y]) {
+                    pq.push({grid[x + 1][y], {x + 1, y}});
+                }
+                // up
+                if(x - 1 >= 0 && !visited[x - 1][y]) {
+                    pq.push({grid[x - 1][y], {x - 1, y}});
+                }
+                // left
+                if(y - 1 >= 0 && !visited[x][y - 1]) {
+                    pq.push({grid[x][y - 1], {x, y - 1}});
+                }
+                // right
+                if(y + 1 < m && !visited[x][y + 1]) {
+                    pq.push({grid[x][y + 1], {x, y + 1}});
+                }
+                
+                cnt++;
+            }
+            
+            ans[query.second] = cnt;
+        }
+        
+        return ans;
+    }
+};
