f38dd9bf1a7538a36bf96e2d9cde88cdd6b8e975
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2023-05-22 18:07:21 +0530

    Time: 343 ms (69.36%), Space: 108 MB (22.18%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,18 @@
+function memoize(fn) {
+    
+   const cache = {};
+  
+   return function(...args) {
+    const key = JSON.stringify(args);
+    
+    if (key in cache) {
+      return cache[key];
+    }
+    
+    const result = fn.apply(this, args);
+    cache[key] = result;
+    
+    return result;
+  }
+  
+}
