08cb393e3cae52c667bfe1fa66d8ddfb361ee57b
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2023-05-29 15:51:34 +0530

    Time: 59 ms (54.58%), Space: 41.6 MB (88.38%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let cnt = 0;
+    return function(...args){
+        if(cnt > 0) {
+            return undefined;
+        }
+        cnt++;
+        
+        return fn.apply(this, args);
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
